04c157aada6293647f921f6f72091a5984af262b
kelbyhubert <horizonhubert@gmail.com>
2021-12-24 19:26:23 +0700

    johanes jancok

--- app/src/main/java/com/example/quizmad/DataAccess/DatabaseHandler.java
+++ app/src/main/java/com/example/quizmad/DataAccess/DatabaseHandler.java
@@ -19,6 +19,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     //tes
     //wibu
     // pantat
+    // joh
     private static final String TAG = "DatabaseHandler";
 
     public static final String DATABASE_NAME = "BLUE_DOLL_DATABASE";
